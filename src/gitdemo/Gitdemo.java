8b508d6e7fe32d066e3c2e7dec111abf8373132b
Bhupesh <sanwaria@sheridancollege.ca>
2019-05-13 13:12:22 -0400

    New Repo for the Git

---
+++ src/gitdemo/Gitdemo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo;
+
+/**
+ *
+ * @author New
+ */
+public class Gitdemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] a=new int[5];
+        a[0]= 1;
+        a[1]=2;
+        a[2]=3;
+        a[3]=4;
+        a[4]=5;
+        for(int i=0;i<a.length;i++)
+        {
+              System.out.println(+a[i]);
+        }
+    }
+    
+}
